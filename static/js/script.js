70bc1970997f538cf840721396070433a062f4e6
NtinosNG <ntinosng@gmail.com>
2019-06-06 15:44:26 +0300

    created scroll-to-top button

--- static/js/script.js
+++ static/js/script.js
@@ -3,3 +3,15 @@
 $('.menu .item')
 .tab();
 
+// Adjust Scroll-to-Top button visibility
+window.onscroll = function() {
+	scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    document.getElementById("myScrollBtn").style.display = "block";
+  } else {
+    document.getElementById("myScrollBtn").style.display = "none";
+  }
+}
